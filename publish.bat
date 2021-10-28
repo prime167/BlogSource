cd98775d27e7559d2dd062ea654bf167ac5f069e
codest <3722845+prime167@users.noreply.github.com>
2021-10-28 10:47:17 +0800

    update

--- publish.bat
+++ publish.bat
@@ -1,9 +1,11 @@
 @echo off
-echo "签入 markdown"
-git add . && git ci -m update && git push
 echo "hugo 生成 html"
 hugo
-cd public
+
+echo "签入 markdown"
+git add . && git ci -m update && git push
+
 echo "发布"
+cd public
 git add . && git ci -m update && git push
 cd ..
